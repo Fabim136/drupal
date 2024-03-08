053b8bce3651b03a2263e02d1b551fc4b93088f9
Fabim136 <fabianojunior136@gmail.com>
2024-03-08 07:45:55 -0300

    criando temas customizados para fins de teste

---
+++ web/modules/custom/fabiano_manual_course/js/fabiano-manual-course.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Fabiano Junior behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fabianoManualCourseFabianoManualCourse = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
